91322ddc967d3856d46165884d815d9a0e7fbb56
dougbthompson <doug.askaar@gmail.com>
2018-08-27 10:29:52 -0700

    20180827 10:29

--- install_postgres.sh
+++ install_postgres.sh
@@ -3,13 +3,15 @@
 # Postgres install for "Postgres 9.6 + PostGIS 2.3"
 
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt xenial-pgdg main" >> /etc/apt/sources.list' 
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 sudo apt-get update
 sleep 5
 
 sudo apt-get install -y postgresql-9.6
 
-sudo -u postgres createuser -P gisdata (password for gisdata: "gisdata")
+sudo -u postgres createuser gisdata
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'gisdata';"
 sudo -u postgres createdb -O gisdata gisdata
 
 sudo apt install -y postgis postgresql-9.6-postgis-2.3
